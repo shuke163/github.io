d3227d8fc8daa88949f3ffa8685effe0cad262c3
shuke <shuke_163@163.com>
2020-06-27 21:06:23 +0800

    [update] 修改build.sh脚本

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# p = picture
+if [[ $# -eq 1 ]] && [[ $1 == "p" ]]; then
+    sed -i "" 's/post_asset_folder\: true/post_asset_folder\: false/' _config.yml
+fi
+
 hexo clean
 hexo g
 hexo d
